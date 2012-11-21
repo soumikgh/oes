ae317802aa29b6b945730361779cc2acdaa7f80b
Soumik Ghosh <contactSoumik@gmail.com>
2012-11-21 12:39:00 +0530

    Initial exam.php

---
+++ exam.php
@@ -0,0 +1,36 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION['user_id']))
+{
+	die('You are not logged in. Please <a href="./">log in</a>.');
+}
+
+$title='Questions - Online examination system';
+require('./template/header.php');
+require_once('./includes/db_connect.php');
+
+$db = db_connect();
+$qr = $db->prepare("SELECT q_body, q_ans1, q_ans2, q_ans3, q_ans4 FROM questions WHERE q_id = ?");
+$qs = array();
+
+echo '<form name="questions" method="post" action="">';
+
+for ($i=1; $i<=5; $i++)
+{
+	while(in_array($qno = rand(1, 10), $qs)); //Find a number not in the array
+	$qs[] = $qno; //Push the number into the array
+	
+	$qr->execute(array($qno));
+	$result = $qr->fetch(PDO::FETCH_ASSOC);
+	echo '<div class="question">' . $result['q_body'] . '</div>';
+	echo '<div class="answer">';
+	for ($j=1; $j<=4; $j++)
+	{
+		echo '<input type="radio" value="'. $j . '" />' . $result["q_ans$j"];
+	}
+	echo '</div>';
+	
+}
+?>
