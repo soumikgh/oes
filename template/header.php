6ab4fee5e091106c3851c64981879593f4dfb075
Soumik Ghosh <contactSoumik@gmail.com>
2012-11-21 19:23:08 +0530

    Added validation JS

--- template/header.php
+++ template/header.php
@@ -1,6 +1,7 @@
 <html>
 	<head>
 		<link rel="stylesheet" type="text/css" href="./style.css" />
+		<script type="text/javascript" src="./js/validate.js"></script>
 		<title><?php echo $title; ?></title>
 	</head>
 	<body>
