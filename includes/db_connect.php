cb813aca581590cf075cd607e25bcfbe4b9ce0e8
Soumik Ghosh <contactSoumik@gmail.com>
2012-12-09 18:00:32 +0530

    Disable emulation of prepared statements

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -2,6 +2,8 @@
 function db_connect()
 {
 	$db = new PDO('mysql:host=localhost;dbname=oes', 'root', '');
+	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	return($db);
 }
 ?>
